43423a8233d838b0dce4e91b2f99a8ff42cbe34e
Abstract-ak <ash.king0205@gmail.com>
2025-05-25 23:19:37 +0530

    Refactor server and preview configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Ahead-soft/",
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: "5173",
+  },
+
+  preview: {
+    port: 5173,
+  },
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
